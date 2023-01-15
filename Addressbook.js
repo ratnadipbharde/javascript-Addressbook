5b0a8e41f4dc42dd89a745da96a2980349234a4c
ratnadipbharde <ratnadipbharde@gmail.com>
2023-01-15 14:08:40 +0530

    refactor function printContact

--- Addressbook.js
+++ Addressbook.js
@@ -1,12 +1,12 @@
-var {contacts} = require('./model/Contact');
-var contact=new contacts();
-contact=createContact();
+var { contacts } = require('./model/Contact');
+var contact = new contacts();
+contact = createContact();
 printContact(contact);
 
-function createContact(){
-    var {contacts} = require('./model/Contact');
+function createContact() {
+    var { contacts } = require('./model/Contact');
     const prompt = require('prompt-sync')();
-    var contact=new contacts();
+    var contact = new contacts();
     contact.setFirstName(prompt("First Name : "));
     contact.setLastName(prompt("Last Name : "));
     contact.setAddress(prompt("Address : "));
@@ -18,17 +18,13 @@ function createContact(){
     return contact;
 }
 
-function printContact(Contact){
-    var {contacts} = require('./model/Contact');
-    var contact=new contacts();
-    contact=Contact;
-    console.log("\n\nFirst Name : "+contact.getFirstName());
-    console.log("Last Name : "+contact.getLastName());
-    console.log("Address: "+contact.getAddress());
-    console.log("City : "+contact.getCity());
-    console.log("State : "+contact.getState());
-    console.log("State : "+contact.getZip());
-    console.log("State : "+contact.getPhoneNumber());
-    console.log("State : "+contact.getEmail());
-    
+function printContact(contact= new contacts()) {
+    console.log("\n\nFirst Name : " + contact.getFirstName());
+    console.log("Last Name : " + contact.getLastName());
+    console.log("Address: " + contact.getAddress());
+    console.log("City : " + contact.getCity());
+    console.log("State : " + contact.getState());
+    console.log("State : " + contact.getZip());
+    console.log("State : " + contact.getPhoneNumber());
+    console.log("State : " + contact.getEmail());
 }
