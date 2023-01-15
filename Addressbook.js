b95b4a1c8cd28215a52a3df90f6e5a6a50f67947
ratnadipbharde <ratnadipbharde@gmail.com>
2023-01-16 00:14:28 +0530

    Added code for edit contact

--- Addressbook.js
+++ Addressbook.js
@@ -4,6 +4,7 @@ const SHOW_ADDRESSBOOK = '1';
 const ADD_CONTACT = '2';
 const EDIT_CONTACT = '3';
 const DELETE_CONTACT = '4';
+
 const EXIT = '0';
 const CONTINUE = '1';
 const RETURN_TO_MAIN = '2';
@@ -24,16 +25,96 @@ addressbook.push(c1);
 addressbook.push(c2);
 
 while (true) {
-    console.log("\n\n0. Exit\n1. show Addressbook\n2. Add Contact")
+    console.log("\n\n0. Exit\n1. show Addressbook\n2. Add Contact\n3. Edit Contact")
     var number = prompt("Enter your choice : ")
     switch (number) {
         case SHOW_ADDRESSBOOK: showAddressbook();
             break;
         case ADD_CONTACT: addContact();
             break;
+        case EDIT_CONTACT: editContact();
+            break;
         case EXIT: return false;
         default: console.log("invalid input...")
     }
+}
+
+function editContact() {
+    var fname = prompt("Enter First Name : ");
+    addressbook.filter(contact => {
+        if (contact.firstName == fname) {
+            contact.printContact(contact);
+            console.log("\n1. First Name\n2.Last Name\n3. Address\n4. City\n5. State\n6. Zip\n7. Phone Number\n8. Email id");
+            let number = prompt("Enter your choice to edit contact")
+            switch (key) {
+                case FIRST_NAME:
+                    try {
+                        contact.setFirstName(prompt("First Name : "));
+                    } catch (error) {
+                        console.error(error);
+                    }
+                    break;
+
+                case LAST_NAME:
+                    try {
+                        contact.setLastName(prompt("Last Name : "));
+                    } catch (error) {
+                        console.error(error);
+                    }
+                    break;
+
+                case ADDRESS:
+                    try {
+                        contact.setAddress(prompt("Address : "))
+                    } catch (error) {
+                        console.error(error);
+                    }
+                    break;
+
+                case CITY:
+                    try {
+                        contact.setCity(prompt("City : "));
+                    } catch (error) {
+                        console.error(error);
+                    }
+
+                    break;
+                case STATE:
+                    try {
+                        contact.setState(prompt("State : "));
+                    } catch (error) {
+                        console.error(error);
+                    }
+
+                    break;
+                case ZIP:
+                    try {
+                        contact.setZip(prompt("Zip : "));
+                    } catch (error) {
+                        console.error(error);
+                    }
+
+                    break;
+                case PHONE_NUMBER:
+                    try {
+                        contact.setPhoneNumber(prompt("Phone Number : "));
+                    } catch (error) {
+                        console.error(error);
+                    }
+
+                    break;
+                case EMAIL:
+                    try {
+                        contact.setEmail(prompt("Email : "));
+                    } catch (error) {
+                        console.error(error);
+                    }
+                    break;
+
+                default: console.log("invalid input...")
+            }
+        }
+    })
 }
 
 function addContact() {
@@ -107,6 +188,5 @@ function createContact() {
     } catch (error) {
         console.error(error);
     }
-
     return contact;
 }
